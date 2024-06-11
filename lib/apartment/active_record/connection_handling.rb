f1a96fd2d73576401296daa52cec57a996db7727
Gokul-Edcast <114460946+Gokul-Edcast@users.noreply.github.com>
2024-06-11 15:23:02 +0530

    Comment connected_to

--- lib/apartment/active_record/connection_handling.rb
+++ lib/apartment/active_record/connection_handling.rb
@@ -6,17 +6,17 @@ module ActiveRecord # :nodoc:
     # switches to the same tenant as before the connection switching. This problem is more evident when
     # using read replica in Rails 6
     module ConnectionHandling
-      def connected_to_with_tenant(role: nil, prevent_writes: false, &blk)
-        current_tenant = Apartment::Tenant.current
+      # def connected_to_with_tenant(role: nil, prevent_writes: false, &blk)
+      #   current_tenant = Apartment::Tenant.current
 
-        connected_to_without_tenant(role: role, prevent_writes: prevent_writes) do
-          Apartment::Tenant.switch!(current_tenant)
-          yield(blk)
-        end
-      end
+      #   connected_to_without_tenant(role: role, prevent_writes: prevent_writes) do
+      #     Apartment::Tenant.switch!(current_tenant)
+      #     yield(blk)
+      #   end
+      # end
 
-      alias connected_to_without_tenant connected_to
-      alias connected_to connected_to_with_tenant
+      # alias connected_to_without_tenant connected_to
+      # alias connected_to connected_to_with_tenant
     end
   end
 end
